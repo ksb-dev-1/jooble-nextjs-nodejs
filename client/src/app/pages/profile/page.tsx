e114d9fbdaf07712c9fe0c9bf4906f1b65ace95e
ksb-dev-1 <ksbabaleshwar@gmail.com>
2024-04-22 00:05:15 +0530

    profile page UI change

--- client/src/app/pages/profile/page.tsx
+++ client/src/app/pages/profile/page.tsx
@@ -23,31 +23,29 @@ const ProfilePage: React.FC = () => {
         {isFetching && <ProfileSkeleton />}
         {isSuccess && <ProfileInfoNoSSR user={data.user} />}
       </div>
-      <div className="flex flex-col md:flex-row w-full mt-4 md:mt-8">
-        <div className="hidden md:flex flex-wrap md:flex-col bg-white shadow-1 rounded-[var(--r1)] p-4 md:p-8 w-full md:w-[225px] sticky top-[6.5rem] h-fit">
-          <p className="font-bold px-2 sm:px-4 mb-2 hidden md:block">
-            Quick links
-          </p>
-          <p className="rounded-[var(--r1)] px-2 sm:px-4 py-2 mb-2 cursor-pointer hover:bg-[#f8f8f8]">
+      <div className="flex flex-col md:flex-row w-full mt-4 sm:mt-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 md:flex md:flex-wrap md:flex-col bg-white shadow-1 rounded-[var(--r1)] p-4 md:p-8 w-full md:w-[225px] md:sticky top-[6.5rem] h-fit">
+          <p className="font-bold hidden md:block text-lg ml-4">Quick links</p>
+          <p className="rounded-[var(--r1)] py-2 pl-4 cursor-pointer hover:bg-[#f8f8f8] border md:border-none text-center md:text-start text-sm md:text-base">
             Key skills
           </p>
-          <p className="rounded-[var(--r1)] px-2 sm:px-4 py-2 mb-2 cursor-pointer hover:bg-[#f8f8f8]">
+          <p className="rounded-[var(--r1)] py-2 pl-4 cursor-pointer hover:bg-[#f8f8f8] border md:border-none text-center md:text-start text-sm md:text-base">
             Projects
           </p>
-          <p className="rounded-[var(--r1)] px-2 sm:px-4 py-2 mb-2 cursor-pointer hover:bg-[#f8f8f8]">
+          <p className="rounded-[var(--r1)] py-2 pl-4 cursor-pointer hover:bg-[#f8f8f8] border md:border-none text-center md:text-start text-sm md:text-base">
             Education
           </p>
-          <p className="rounded-[var(--r1)] px-2 sm:px-4 py-2 mb-2 cursor-pointer hover:bg-[#f8f8f8]">
+          <p className="rounded-[var(--r1)] py-2 pl-4 cursor-pointer hover:bg-[#f8f8f8] border md:border-none text-center md:text-start text-sm md:text-base">
             Certifications
           </p>
-          <p className="rounded-[var(--r1)] px-2 sm:px-4 py-2 mb-2 cursor-pointer hover:bg-[#f8f8f8]">
+          <p className="rounded-[var(--r1)] py-2 pl-4 cursor-pointer hover:bg-[#f8f8f8] border md:border-none text-center md:text-start text-sm md:text-base">
             Career profile
           </p>
-          <p className="rounded-[var(--r1)] px-2 sm:px-4 py-2 cursor-pointer hover:bg-[#f8f8f8]">
+          <p className="rounded-[var(--r1)] py-2 pl-4 cursor-pointer hover:bg-[#f8f8f8] border md:border-none text-center md:text-start text-sm md:text-base">
             Personal details
           </p>
         </div>
-        <div className="flex-grow mt-4 md:mt-0 md:ml-8 h-fit">
+        <div className="flex-grow mt-4 sm:mt-8 md:mt-0 md:ml-8 h-fit">
           {/* Key skills */}
           <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8">
             <div className="flex items-center">
@@ -61,7 +59,7 @@ const ProfilePage: React.FC = () => {
             <p className="text-blue-600 font-semibold cursor-pointer">Add</p>
           </div>
           {/* Projects */}
-          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-8">
+          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-4 sm:mt-8">
             <div className="flex items-center">
               <p className="font-semibold mr-2">Projects</p>
               <p className="relative h-[30px] w-[30px] rounded-full text-white bg-blue-600 hover:bg-blue-500 cursor-pointer">
@@ -73,7 +71,7 @@ const ProfilePage: React.FC = () => {
             <p className="text-blue-600 font-semibold cursor-pointer">Add</p>
           </div>
           {/* Education */}
-          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-8">
+          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-4 sm:mt-8">
             <div className="flex items-center">
               <p className="font-semibold mr-2">Education</p>
               <p className="relative h-[30px] w-[30px] rounded-full text-white bg-blue-600 hover:bg-blue-500 cursor-pointer">
@@ -85,7 +83,7 @@ const ProfilePage: React.FC = () => {
             <p className="text-blue-600 font-semibold cursor-pointer">Add</p>
           </div>
           {/* Certifications */}
-          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-8">
+          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-4 sm:mt-8">
             <div className="flex items-center">
               <p className="font-semibold mr-2">Certifications</p>
               <p className="relative h-[30px] w-[30px] rounded-full text-white bg-blue-600 hover:bg-blue-500 cursor-pointer">
@@ -97,7 +95,7 @@ const ProfilePage: React.FC = () => {
             <p className="text-blue-600 font-semibold cursor-pointer">Add</p>
           </div>
           {/* Career profile */}
-          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-8">
+          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-4 sm:mt-8">
             <div className="flex items-center">
               <p className="font-semibold mr-2">Career profile</p>
               <p className="relative h-[30px] w-[30px] rounded-full text-white bg-blue-600 hover:bg-blue-500 cursor-pointer">
@@ -109,7 +107,7 @@ const ProfilePage: React.FC = () => {
             <p className="text-blue-600 font-semibold cursor-pointer">Add</p>
           </div>
           {/* Personal details */}
-          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-8">
+          <div className="flex items-center justify-between bg-white shadow-1 rounded-[var(--r1)] p-4 sm:p-8 mt-4 sm:mt-8">
             <div className="flex items-center">
               <p className="font-semibold mr-2">Personal details</p>
               <p className="relative h-[30px] w-[30px] rounded-full text-white bg-blue-600 hover:bg-blue-500 cursor-pointer">
--- client/src/components/EditProfileForm.tsx
+++ client/src/components/EditProfileForm.tsx
@@ -224,22 +224,22 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
               />
               <label
                 htmlFor="image"
-                className="relative cursor-pointer block w-[75px] sm:w-[100px] h-[75px] sm:h-[100px] rounded-full overflow-hidden border border-slate-400"
+                className="relative cursor-pointer block w-[100px] sm:w-[125px] h-[100px] sm:h-[125px] rounded-full overflow-hidden border border-slate-400"
                 onMouseOver={() => setOnHoverImage(true)}
                 onMouseLeave={() => setOnHoverImage(false)}
               >
                 {onHoverImage && (
-                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.5)] text-white rounded-full w-[4.15rem] sm:w-[5.5rem] h-[4.15rem] sm:h-[5.5rem] flex flex-col items-center justify-center">
-                    <FaUpload className="sm:text-xl mb-1 sm:mb-2" />
+                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.5)] text-white rounded-full w-[5.65rem] sm:w-[7rem] h-[5.65rem] sm:h-[7rem] flex flex-col items-center justify-center">
+                    <FaUpload className="sm:text-xl mb-1 sm" />
                     <span className="text-[0.6rem] sm:text-xs font-medium">
                       Upload
                     </span>
                   </div>
                 )}
-                {!onHoverImage && !values.image && (
-                  <BiSolidUserCircle className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[4.85rem] sm:text-[6.5rem] text-slate-300" />
+                {!onHoverImage && values.image && (
+                  <BiSolidUserCircle className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[110px] sm:text-[135px] text-slate-300" />
                 )}
-                {values.image && !onHoverImage && (
+                {!values.image && !onHoverImage && (
                   <Image
                     src={values.image}
                     alt="pic"
@@ -252,19 +252,22 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
               </label>
             </div>
 
-            <div className="flex flex-col mt-8 rounded-[var(--r1)]">
+            <div className="flex flex-col mt-4 rounded-[var(--r1)]">
               <p className="font-semibold text-sm sm:text-base">
                 Available to join
               </p>
-              <div className="flex flex-wrap mt-2 rounded-[var(--r1)]">
+              <p className="text-slate-500 text-xs sm:text-sm">
+                Lets recruiters know your availability to join
+              </p>
+              <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 mt-2 rounded-[var(--r1)] border border-slate-400 p-2 sm:p-4">
                 {values.available_to_join === "less than 15 days" ? (
-                  <span className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 mr-2 mb-2 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white flex items-center">
+                  <span className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white flex items-center justify-center">
                     <FaLessThan className="mr-1 mt-[2px] text-[0.65rem] text-white" />
                     15 Days
                   </span>
                 ) : (
                   <span
-                    className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-300 mr-2 mb-2 cursor-pointer flex items-center hover:bg-slate-100"
+                    className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-400 cursor-pointer flex items-center justify-center hover:bg-slate-100"
                     onClick={(e: any) =>
                       setValues({
                         ...values,
@@ -277,12 +280,12 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
                   </span>
                 )}
                 {values.available_to_join === "15 days" ? (
-                  <span className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 mr-2 mb-2 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
+                  <span className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 text-center cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
                     15 Days
                   </span>
                 ) : (
                   <span
-                    className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-300 mr-2 mb-2 cursor-pointer hover:bg-slate-100"
+                    className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-400 text-center cursor-pointer hover:bg-slate-100"
                     onClick={(e: any) =>
                       setValues({
                         ...values,
@@ -294,12 +297,12 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
                   </span>
                 )}
                 {values.available_to_join === "1 month" ? (
-                  <span className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 mr-2 mb-2 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
+                  <span className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 text-center cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
                     1 Month
                   </span>
                 ) : (
                   <span
-                    className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-300 mr-2 mb-2 cursor-pointer hover:bg-slate-100"
+                    className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-400 text-center cursor-pointer hover:bg-slate-100"
                     onClick={(e: any) =>
                       setValues({
                         ...values,
@@ -311,12 +314,12 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
                   </span>
                 )}
                 {values.available_to_join === "2 months" ? (
-                  <span className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 mr-2 mb-2 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
+                  <span className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 text-center cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
                     2 Months
                   </span>
                 ) : (
                   <span
-                    className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-300 mr-2 mb-2 cursor-pointer hover:bg-slate-100"
+                    className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-400 text-center cursor-pointer hover:bg-slate-100"
                     onClick={(e: any) =>
                       setValues({
                         ...values,
@@ -328,12 +331,12 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
                   </span>
                 )}
                 {values.available_to_join === "3 months" ? (
-                  <span className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 mr-2 mb-2 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
+                  <span className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 text-center cursor-pointer bg-blue-600 hover:bg-blue-500 text-white">
                     3 Months
                   </span>
                 ) : (
                   <span
-                    className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-300 mr-2 mb-2 cursor-pointer hover:bg-slate-100"
+                    className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-400 text-center cursor-pointer hover:bg-slate-100"
                     onClick={(e: any) =>
                       setValues({
                         ...values,
@@ -345,13 +348,13 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
                   </span>
                 )}
                 {values.available_to_join === "more than 3 months" ? (
-                  <span className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 mr-2 mb-2 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white flex items-center">
+                  <span className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-blue-600 cursor-pointer bg-blue-600 hover:bg-blue-500 text-white flex items-center justify-center">
                     <FaGreaterThan className="mr-1 mt-[2px] text-[0.65rem] text-white" />
                     3 Months
                   </span>
                 ) : (
                   <span
-                    className="text-xs sm:text-sm py-1 sm:py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-300 mr-2 mb-2 cursor-pointer flex items-center hover:bg-slate-100"
+                    className="text-xs sm:text-sm py-2 px-2 sm:px-4 rounded-[var(--r1)] border border-slate-400 cursor-pointer flex items-center justify-center hover:bg-slate-100"
                     onClick={(e: any) =>
                       setValues({
                         ...values,
@@ -412,12 +415,12 @@ const EditProfileForm = forwardRef<HTMLDivElement, UserProps>(
               </div>
             </div>
 
-            <div className="flex-grow box-border">
+            <div className="flex-grow box-border mt-4">
               <input
                 type="text"
                 name="mobile_no"
                 value={values.mobile_no}
-                className="border border-slate-400 rounded-[var(--r1)] px-3 sm:px-4 py-2 sm:py-3 focus:outline-blue-600 placeholder:text-sm placeholder:text-slate-500 focus:placeholder:text-transparent mt-4 w-[100%] text-sm sm:text-base"
+                className="border border-slate-400 rounded-[var(--r1)] px-3 sm:px-4 py-2 sm:py-3 focus:outline-blue-600 placeholder:text-sm placeholder:text-slate-500 focus:placeholder:text-transparent w-[100%] text-sm sm:text-base"
                 onChange={handleChange}
                 placeholder="Mobile"
               />
